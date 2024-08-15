434550d7560283f81a73413950f2d8d67980ca98
Au Ra Tha <auratha1000@gmail.com>
2024-08-15 21:44:04 +0630

    Finished the design

---
+++ pulse/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily : {
+        'work-sans' :["Work Sans", "sans-serif"]
+      }
+    },
+  },
+  plugins: [],
+}
+
